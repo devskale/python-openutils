5c1eeb6beffae7bf2a122d03352a8fa984d46e7e
devskale <dev@skale.dev>
2026-02-19 22:52:01 +0100

    chore: remove __pycache__ from index and update .gitignore

--- packages/uniinfer/build/lib/uniinfer/examples/webdemo/config.js
+++
@@ -1,8 +0,0 @@
-// Provide a global config object that can be updated from the UI
-window.uniinferConfig = {
-  // Default to current origin (e.g. http://localhost:8123) if running locally, 
-  // otherwise fallback or user can configure.
-  apiBaseUrl: window.location.origin, 
-  apiKey: "",
-  maxTokens: 4000,
-};
